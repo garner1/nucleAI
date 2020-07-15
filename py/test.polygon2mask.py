700c35e8e0575466386ce20802a95c475a5670a1
garner1 <silvano.garnerone@gmail.com>
2020-07-15 18:39:03 +0200

    modification: remove decompression in python script because it mixes directories

--- py/test.polygon2mask.py
+++ py/test.polygon2mask.py
@@ -30,34 +30,26 @@ from tqdm import tqdm
 # In[ ]:
 
 
-datadir = '/home/garner1/Work/dataset/tcga_polygons/LUAD'
-samples = glob.glob(datadir+'/*.gz')
-print('There are '+str(len(samples))+' samples')
-
-
-# In[ ]:
-
-
 features = ['centroid_x','centroid_y','area','eccentricity','orientation','perimeter','solidity']
 num_cores = multiprocessing.cpu_count() # numb of cores
 
-#for sample in glob.glob(datadir+'/*.gz/*.gz'): # for each sample compressed file
 sample = sys.argv[1] #~/Work/dataset/tcga_polygons/LUAD/*.gz/*.gz
 ID = os.path.basename(sample).split(sep='.')[0] #get sample ID
 print(ID)
 dirname = os.path.dirname(sample) #get the sample directory
 print(dirname)
 
-get_ipython().system('tar -xf $sample')
-get_ipython().system("mv './luad_polygon/' $dirname")
+# get_ipython().system('tar -xf $sample')
+# get_ipython().system("mv './luad_polygon/' $dirname") # this part might create problems when run in parallel because dirs are not correctly assigned
+
 patchlist = glob.glob(dirname+'/*_polygon/*.svs/*.csv') #get the list of patches    
 print('There are '+str(len(patchlist))+' patches')
 
-print('Showing the patches as png files...')
-Parallel(n_jobs=num_cores)(delayed(show_patches_parallel)(filename) for filename in tqdm(patchlist) if ~pd.read_csv(filename).empty)
+# print('Showing the patches as png files...')
+# Parallel(n_jobs=num_cores)(delayed(show_patches_parallel)(filename) for filename in tqdm(patchlist) if ~pd.read_csv(filename).empty)
         
-# print('Calculating the morphometry...')
-# Parallel(n_jobs=num_cores)(
-#     delayed(measure_patch_of_polygons)(filename,features) for filename in tqdm(patchlist) if ~pd.read_csv(filename).empty
-# )
+print('Calculating the morphometry...')
+Parallel(n_jobs=num_cores)(
+    delayed(measure_patch_of_polygons)(filename,features) for filename in tqdm(patchlist) if ~pd.read_csv(filename).empty
+)
 print('Done!')

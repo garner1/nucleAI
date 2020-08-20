33ba3b8a9d43078ecb6fd317ba5a017419fad0a1
garner1 <silvano.garnerone@gmail.com>
2020-08-20 09:31:17 +0200

    minor

--- script.sh
+++ script.sh
@@ -4,15 +4,26 @@ dirSample=$1 # the full path to the sample dir
 
 echo ${dirSample}
 cd ${dirSample}
-echo "Uncompress"
-[[ -d ${dirSample}/*polygon ]] || tar -xf *.gz -C ${dirSample}  
+
+if [ ! -d ${dirSample}/*polygon ]
+then
+    echo "Uncompress"
+    tar -xf *.gz -C ${dirSample}
+fi
 
 echo "Generate the masks"
 parallel "[[ -f {}.morphometrics.pkl ]] || /usr/local/share/anaconda3/bin/ipython /home/garner1/Work/pipelines/nucleAI/py/test.polygon2mask.patch.morphometry.py {}" ::: ${dirSample}/*_polygon/TCGA-*.svs/*-features.csv
 
-echo "Generate the descriptors"
-#rm -f ${dirSample}/*.features.pkl
-[[ -f ${dirSample}/*.freq*.covdNN*.features.pkl ]] || /usr/local/share/anaconda3/bin/ipython /home/garner1/Work/pipelines/nucleAI/py/test.mask2descriptor.py 10 50 ${dirSample}
+if [ ! -f ${dirSample}/*.freq*.covdNN*.features.pkl ]
+then
+    echo "Generate the descriptors"
+    /usr/local/share/anaconda3/bin/ipython /home/garner1/Work/pipelines/nucleAI/py/test.mask2descriptor.py 10 50 ${dirSample}
+fi
+
+echo "Clean up"
+parallel "rm {}" ::: ${dirSample}/*_polygon/TCGA-*.svs/*-features.csv
+
+
 
 
 
--- scripts.sh
+++ scripts.sh
@@ -2,7 +2,7 @@
 
 for s in  `ls -d /media/garner1/hdd2/TCGA_polygons/*/TCGA-*.svs.tar.gz | shuf`
 do
-    time bash /home/garner1/Work/pipelines/nucleAI/script.sh ${s}
+    time bash /home/garner1/Work/pipelines/nucleAI/script_test.sh ${s}
 done
 
 

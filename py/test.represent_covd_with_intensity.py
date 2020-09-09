4c5dacf0ce2eea781b8a35c1834cea5d300a05d1
garner1 <silvano.garnerone@gmail.com>
2020-09-09 11:11:29 +0200

    modification: fixed bug with respect to xy centroid coordinates

--- py/test.represent_covd_with_intensity.py
+++ py/test.represent_covd_with_intensity.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from __future__ import print_function
-
 from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use('Agg') # to not display figure while using ssh 
 import matplotlib.patches as mpatches
-get_ipython().run_line_magic('matplotlib', 'inline')
 
 import sys
 sys.path.insert(0, '../py')
@@ -30,7 +29,6 @@ import os
 import timeit
 import random
 
-import pyvips
 import timeit
 import multiprocessing
 from joblib import Parallel, delayed
@@ -40,7 +38,9 @@ from tqdm import tqdm
 import umap
 import seaborn as sns; sns.set()
 
-patches = glob.glob('/home/garner1/pipelines/nucleAI/data/TCGA-A2-A0CK-01Z-00-DX1/*.pkl')
+from sklearn.cluster import KMeans
+
+patches = glob.glob('/media/garner1/hdd2/TCGA_polygons/BRCA/TCGA-A2-A0CK-01Z-00-DX1.*.svs.tar.gz/brca_polygon/TCGA-A2-A0CK-01Z-00-DX1.*.svs/*.intensity_features.pkl')
 
 reducer = umap.UMAP(n_components=2,min_dist=0,n_neighbors=10)
 intensity_features = []
@@ -51,25 +51,30 @@ for patch in patches[:]:
     intensity_features.extend(lista)
     infile.close()
 
-list_of_features = random.sample(intensity_features,100000)
-pos = np.array([f[0] for f in list_of_features if f is not None])  
-data_cov = np.array([np.real(sp.linalg.logm(np.cov(f[1],rowvar=False))).flatten() for f in list_of_features if f is not None])  
-data_corrcoef = np.array([np.real(sp.linalg.logm(np.corrcoef(f[1],rowvar=False))).flatten() for f in list_of_features if f is not None])  
+print(len(intensity_features))
+list_of_features = random.sample(intensity_features,10000)
+pos = np.array([f[0] for f in intensity_features if f is not None])  
+print(np.max(pos,axis=0))
+# data_cov = np.array([np.real(sp.linalg.logm(np.cov(f[1],rowvar=False))).flatten() for f in list_of_features if f is not None])  
+# data_corrcoef = np.array([np.real(sp.linalg.logm(np.corrcoef(f[1],rowvar=False))).flatten() for f in list_of_features if f is not None])  
 
-embedding = reducer.fit_transform(data_cov)
-x = embedding[:,0]
-y = embedding[:,1]
-df_plot = pd.DataFrame(dict(x=x, y=y))
-fig, ax = plt.subplots(figsize=(10,10))
-ax = sns.scatterplot(x="x", y="y", data=df_plot)
-plt.savefig('cov.png')
+# # UMAP representation
+# embedding = reducer.fit_transform(data_cov)
+# x = embedding[:,0]
+# y = embedding[:,1]
+# df_plot = pd.DataFrame(dict(x_umap=x, y_umap=y))
+# df_plot['x'] = pos[:,0]; df_plot['y'] = pos[:,1]
 
-embedding = reducer.fit_transform(data_corrcoef)
-x = embedding[:,0]
-y = embedding[:,1]
-df_plot = pd.DataFrame(dict(x=x, y=y))
-fig, ax = plt.subplots(figsize=(10,10))
-ax = sns.scatterplot(x="x", y="y", data=df_plot)
-plt.savefig('corrcoef.png')
+# # K-means classification
+# kmeans = KMeans(n_clusters=2, random_state=0).fit(embedding)
+# df_plot['cluster'] = kmeans.labels_
+
+# # Show the cluster to study
+# fig, ax = plt.subplots(figsize=(10,10))
+# ax = sns.scatterplot(x="x", y="y",hue='cluster',s=10, data=df_plot)
+# plt.savefig('cov.png')
+# # ax = seaborn.FacetGrid(data=df_plot[['x_umap','y_umap','cluster']],hue='cluster')
+# # ax.map(plt.scatter, 'x_umap', 'y_umap',s=10).add_legend()
+
 
 

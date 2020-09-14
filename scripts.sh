0601af01bfaa712ff348fff57559f0e80077dcce
garner1 <silvano.garnerone@gmail.com>
2020-09-14 10:33:15 +0200

    minor

--- scripts.sh
+++ scripts.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-for s in  `ls -d /media/garner1/hdd2/TCGA_polygons/*/TCGA-*.svs.tar.gz | shuf`
-do
-    time bash /home/garner1/Work/pipelines/nucleAI/script_test.sh ${s}
-done
+for svs in `ls /media/garner1/hdd2/BRCA_svs/*/*.svs`; do bash script.intensity.sh $svs; done
+
+#############################################
+# for s in  `ls -d /media/garner1/hdd2/TCGA_polygons/*/TCGA-*.svs.tar.gz | shuf`
+# do
+#     time bash /home/garner1/Work/pipelines/nucleAI/script_test.sh ${s}
+# done
 
 
 

a5cf3feca5757ea4862aacc47f3aeca2b5bdefcb
garner1 <silvano.garnerone@gmail.com>
2020-09-18 09:29:38 +0200

    minor

--- script.intensity.sh
+++ script.intensity.sh
@@ -8,26 +8,31 @@ dirSample='/media/garner1/hdd2/TCGA_polygons/'$cancer_type'/'$samplename'.*.tar.
 
 echo ${cancer_type} $samplename 
 
-if test -d $dirSample; then
-    echo "Uncompress"
-    cd $dirSample
-    tar -xf *.gz -C $PWD
+# if test -d $dirSample; then
+#     echo "Uncompress"
+#     cd $dirSample
+#     tar -xf *.gz -C $PWD
 
-    echo "Generate the intensity features"
-    cp $svsSample ~/local.svs
-    rm -f ${dirSample}/*_polygon/${samplename}.*/*.pkl # clean the directory
-    /usr/local/share/anaconda3/bin/ipython /home/garner1/Work/pipelines/nucleAI/py/test.covd_with_intensity_parallelOverPatches.py $dirSample ~/local.svs
+#     echo "Generate the intensity features"
+#     cp $svsSample ~/local.svs
+#     rm -f ${dirSample}/*_polygon/${samplename}.*/*.pkl # clean the directory
+#     /usr/local/share/anaconda3/bin/ipython /home/garner1/Work/pipelines/nucleAI/py/test.covd_with_intensity_parallelOverPatches.py $dirSample ~/local.svs
 
-    mkdir -p /home/garner1/Work/pipelines/nucleAI/data/features/${cancer_type}/${samplename}
-    mv ${dirSample}/*_polygon/${samplename}.*/*.pkl /home/garner1/Work/pipelines/nucleAI/data/features/${cancer_type}/${samplename}
+#     mkdir -p /home/garner1/Work/pipelines/nucleAI/data/features/${cancer_type}/${samplename}
+#     mv ${dirSample}/*_polygon/${samplename}.*/*.pkl /home/garner1/Work/pipelines/nucleAI/data/features/${cancer_type}/${samplename}
 
-    echo "Clean up"
-    rm ~/local.svs
-    parallel "rm {}" ::: ${dirSample}/*_polygon/TCGA-*.svs/*-features.csv
-fi
+#     echo "Clean up"
+#     rm ~/local.svs
+#     parallel "rm {}" ::: ${dirSample}/*_polygon/TCGA-*.svs/*-features.csv
+# fi
 
 # After the first part has finished run this
-for s in `ls -d ../data/features/BRCA/TCGA-*`; do ~/anaconda3/bin/ipython test.mask2descriptor.py 10 50 ${s}; done
+for s in `ls -d data/features/BRCA/TCGA-*`
+do
+    sample_id=`echo "${s}" | cut -d '/' -f4`
+    # If covds data is not present run the script
+    [ -f data/covds/BRCA/${sample_id}/*.pkl ] || ~/anaconda3/bin/ipython py/test.mask2descriptor.py 10 50 ${s}
+done
 
 #/usr/local/share/anaconda3/bin/ipython py/phase1_step1.mask2descriptor_wo_intensity.py 10 50 data/features_wo_intensity/${cancer_type}/${samplename}
 # for sample in `ls data`; do /usr/local/share/anaconda3/bin/ipython py/phase1_step1.mask2descriptor_with_intensity.py 10 50 data/${sample}; done

38b72914cca7f5a55d51da7a47c6c52115b09e9c
garner1 <silvano.garnerone@gmail.com>
2020-11-12 15:40:59 +0100

    minor

--- scripts.sh
+++ scripts.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 step=1 # if doing first or second step in pipeline
+for svs in `ls /media/garner1/hdd2/svs_LUSC/*/*.svs`; do bash script.intensity.sh $svs $step; done
 
-for svs in `ls /media/garner1/hdd2/svs_GBM/*/*.svs`; do bash script.intensity.sh $svs $step; done
+step=2 # if doing first or second step in pipeline
+for svs in `ls /media/garner1/hdd2/svs_LUSC/*/*.svs`; do bash script.intensity.sh $svs $step; done
 
 #######################################
 # To project in 2d the covds optained so far

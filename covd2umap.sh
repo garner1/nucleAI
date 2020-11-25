ed3b96c9dca2a531f17af11b4129c370809a44f3
garner1 <silvano.garnerone@gmail.com>
2020-11-25 13:16:01 +0100

    script to get the umap projection from descriptors

---
+++ covd2umap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# To project in 2d the covds optained so far
+/usr/local/share/anaconda3/bin/ipython3 py/test.covd_2dProjection.py /media/garner1/hdd2/covds/BLCA
+/usr/local/share/anaconda3/bin/ipython3 py/test.covd_2dProjection.py /media/garner1/hdd2/covds/BRCA
+/usr/local/share/anaconda3/bin/ipython3 py/test.covd_2dProjection.py /media/garner1/hdd2/covds/CESC
+/usr/local/share/anaconda3/bin/ipython3 py/test.covd_2dProjection.py /media/garner1/hdd2/covds/GBM
+/usr/local/share/anaconda3/bin/ipython3 py/test.covd_2dProjection.py /media/garner1/hdd2/covds/LUAD
+/usr/local/share/anaconda3/bin/ipython3 py/test.covd_2dProjection.py /media/garner1/hdd2/covds/LUSC
+/usr/local/share/anaconda3/bin/ipython3 py/test.covd_2dProjection.py /media/garner1/hdd2/covds/PAAD
+/usr/local/share/anaconda3/bin/ipython3 py/test.covd_2dProjection.py /media/garner1/hdd2/covds/PRAD
+/usr/local/share/anaconda3/bin/ipython3 py/test.covd_2dProjection.py /media/garner1/hdd2/covds/SKCM
+/usr/local/share/anaconda3/bin/ipython3 py/test.covd_2dProjection.py /media/garner1/hdd2/covds/UCEC
